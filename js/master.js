d482570fcd7a298bf0884948ffc5a2b3e761505d
chaseweaver <chase.weaver34@gmail.com>
2018-06-01 07:00:32 -0400

    Initial

--- js/master.js
+++ js/master.js
@@ -1,39 +1,40 @@
-/* Change logo name based on screen size */
-$(window).resize(function() {
-    if ($(window).width() < 800) {
-        $("#mHome").html("CHASE WEAVER");
-    } 
-    else {
-        $("#mHome").html("CW");
-    }
-});
+let colors = ['#ffe4e1', '#e8ebc2', '#c6f3f0', '#91bebb', '#ed8788', '#f6cde6'];
 
-/* Apply function to all children of div */
-$(document).ready(function() {
-    $('#mNav').on('click', '> *', function() {
-        var nav = document.getElementById("mNav");
-        var drawer = document.getElementById("mIcon");
-    
-        if (nav.className === "nav") {
-            nav.className += " responsive";
-            drawer.innerHTML = drawer.innerHTML.replace('expand_more', 'expand_less');
-        } else {
-            nav.className = "nav";
-            drawer.innerHTML = drawer.innerHTML.replace('expand_less', 'expand_more');
-        }
-    });
-});
+/* Randomizes accent color on page load */
+window.onload = function() {
+  let accentColor = colors[Math.floor(Math.random() * colors.length)];
+  document.documentElement.style.setProperty('--accent-color', accentColor);
+};
 
-function expand(element, ignore) {
-    var ele = document.getElementById(element);
-    var ig = document.getElementById(ignore);
-    var ig_child = document.getElementById(ignore.children);
+/* Switches navigation bar styles on scroll */
+window.onscroll = function() {
+  let nav = document.getElementById('nav');
+  let divider = document.getElementById('divider');
 
-    $(ele).children().filter(':not(ig_child)').slideToggle();
-
-    if (ig_child.innerHTML === "expand_less") {
-        ig_child.innerHTML = ig_child.innerHTML.replace('expand_less', 'expand_more');
-    } else {
-        ig_child.innerHTML = ig_child.innerHTML.replace('expand_more', 'expand_less');
-    }
+  if (window.pageYOffset >= 20) {
+    nav.className += ' solid';
+    divider.style.setProperty('border-right', '2px solid #fff');
+  } else {
+    nav.className = 'nav';
+    divider.style.setProperty('border-right', '2px solid #000');
+  }
 }
+
+function avatarOnMouseOver(element) {
+  element.setAttribute('src', 'images/avatar_smile.png');
+}
+
+function avatarOnMouseOut(element) {
+  element.setAttribute('src', 'images/avatar.png');
+}
+
+/*
+let parent = $('icon');
+parent.animate({'margin-right' : '-=5px', 'margin-left' : '+=5px'}, 200,
+  function() {
+    parent.animate( {'margin-right' : '+=10px', 'margin-left' : '-=10px'}, 100,
+      function() {
+        parent.animate({'margin-right' : '-=5px', 'margin-left' : '+=5px'}, 200)
+      })
+})
+*/
