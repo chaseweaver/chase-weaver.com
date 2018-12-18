6b0406f9602fe483fc1dba50b6e9ffb4aaa0431b
Chase Weaver <chase.weaver34@gmail.com>
2018-12-18 11:50:14 -0500

    Testing

--- js/master.js
+++ js/master.js
@@ -1,45 +1,10 @@
 /* eslint-disable no-unused-vars, no-undef */
 
-$(function() {
-  // Remove loading fade
-  /*
-  const fadein = document.getElementById('fade-in');
-  fadein.style.background = '#0000';
-  setTimeout(function() { fadein.style.setProperty('display', 'none'); }, 1000);
-  */
- 
-  // Sets random color injected into css
-  document.documentElement.style.setProperty('--accent-color', getRandomColor());
+// Inject navigation panel into page
+$(document).ready(function() {
+  $('#navigation').load('pages/nav.html');
+  $('#nav-style').load('css/nav.css');
+
+  $('#content').load('pages/code.html');
+  $('#content-style').append('css/code.css');
 });
-
-/* Generates a random color for accent */
-function getRandomColor() {
-  const letters = 'ABCDEF'.split('');
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * letters.length)];
-  } 
-  return color;
-}
-
-/* Loads site navigation into each page */
-$(function() {
-  $('#header-placeholder').load('include/header.html');
-});
-
-/* Switches header bar styles on scroll */
-/*
-$(function() {
-  $(window).scroll(function() {
-    const header = document.getElementById('header');
-    const divider = document.getElementById('divider');
-    if (window.pageYOffset >= 20) {
-      header.className += ' solid';
-      divider.style.setProperty('border-right', '2px solid #fff');
-    } else {
-      header.className = 'header';
-      divider.style.setProperty('border-right', '2px solid #000');
-    }
-  });
-}); 
-*/
