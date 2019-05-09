7aa8b9017896afc180273b2bda335de11247db4c
chaseweaver <chase.weaver34@gmail.com>
2019-05-09 02:55:49 -0400

    Testing

--- js/master.js
+++ js/master.js
@@ -12,9 +12,33 @@ $(document).ready(function() {
 
 // Set initial site theme
 $(document).ready(function() {
-	switchTheme('black');
-	switchTheme('red');
+	loadThemeFromCookies();
 });
+
+function setCookie(name, value, days) {
+	let expires = '';
+	if (days) {
+		let date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = '; expires=' + date.toUTCString();
+	}
+	document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+function getCookie(name) {
+	let nameEQ = name + '=';
+	let ca = document.cookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	document.cookie = name + '=; Max-Age=-99999999;';
+}
 
 let themeAccent = 0;
 function cycleThemeAccent() {
@@ -93,32 +117,91 @@ function switchTheme(theme) {
 			return;
 	}
 
+	if (primary != null) {
+		document.documentElement.style.setProperty('--primary', primary);
+		setCookie('primary', primary, 14);
+	}
+
+	if (primaryVar != null) {
+		document.documentElement.style.setProperty('--primaryVar', primaryVar);
+		setCookie('primaryVar', primaryVar, 14);
+	}
+
+	if (secondary != null) {
+		document.documentElement.style.setProperty('--secondary', secondary);
+		setCookie('secondary', secondary, 14);
+	}
+
+	if (secondaryVar != null) {
+		document.documentElement.style.setProperty('--secondaryVar', secondaryVar);
+		setCookie('secondaryVar', secondaryVar, 14);
+	}
+
+	if (background != null) {
+		document.documentElement.style.setProperty('--background', background);
+		setCookie('background', background, 14);
+	}
+
+	if (backgroundVar != null) {
+		document.documentElement.style.setProperty(
+			'--backgroundVar',
+			backgroundVar
+		);
+		setCookie('backgroundVar', backgroundVar, 14);
+	}
+
+	if (text != null) {
+		document.documentElement.style.setProperty('--text', text);
+		setCookie('text', text, 14);
+	}
+
+	if (textInv != null) {
+		document.documentElement.style.setProperty('--textInv', textInv);
+		setCookie('textInv', textInv, 14);
+	}
+
+	if (error != null) {
+		document.documentElement.style.setProperty('--error', error);
+		setCookie('error', error, 14);
+	}
+}
+
+function loadThemeFromCookies() {
+	let primary = getCookie('primary');
 	if (primary != null)
 		document.documentElement.style.setProperty('--primary', primary);
 
+	let primaryVar = getCookie('primaryVar');
 	if (primaryVar != null)
 		document.documentElement.style.setProperty('--primaryVar', primaryVar);
 
+	let secondary = getCookie('secondary');
 	if (secondary != null)
 		document.documentElement.style.setProperty('--secondary', secondary);
 
+	let secondaryVar = getCookie('secondaryVar');
 	if (secondaryVar != null)
 		document.documentElement.style.setProperty('--secondaryVar', secondaryVar);
 
+	let background = getCookie('background');
 	if (background != null)
 		document.documentElement.style.setProperty('--background', background);
 
-	if (backgroundVar != null)
+	let backgroundVar = getCookie('backgroundVar');
+	if (background != null)
 		document.documentElement.style.setProperty(
 			'--backgroundVar',
 			backgroundVar
 		);
 
+	let text = getCookie('text');
 	if (text != null) document.documentElement.style.setProperty('--text', text);
 
+	let textInv = getCookie('textInv');
 	if (textInv != null)
 		document.documentElement.style.setProperty('--textInv', textInv);
 
+	let error = getCookie('error');
 	if (error != null)
 		document.documentElement.style.setProperty('--error', error);
 }
