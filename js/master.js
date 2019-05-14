f4957494fa1747ef67f1ac9eaa613f7063afb66f
chaseweaver <chase.weaver34@gmail.com>
2019-05-14 01:13:39 -0400

    Just breaking everything. This is fine.

--- js/master.js
+++ js/master.js
@@ -2,16 +2,8 @@
 
 // Inject navigation panel into page
 $(document).ready(function() {
-	$('#nav-style').load('css/nav.css');
-	$('#nav-page').load('pages/nav.html');
-
-	if (!getCookie('page')) {
-		$('#pg-style').load('css/home.css', function() {
-			$('#pg-page').load('pages/home.html', function() {
-				setCookie('page', 'home', 14);
-			});
-		});
-	}
+	$('#nav-container').load('pages/nav.html');
+	$('#page-container').load('pages/home.html');
 });
 
 // Set initial site theme
@@ -19,45 +11,21 @@ $(document).ready(function() {
 	loadThemeFromCookies();
 });
 
-$(window).on('load', function() {
-	if ((pg = getCookie('page'))) {
-		$('#pg-style').load(`css/${pg}.css`, function() {
-			$('#pg-page').load(`pages/${pg}.html`);
-		});
-	}
-});
-
-function loadPage(pg) {
-	document.getElementById('content-container').style.opacity = '0';
-	sleep(500).then(() => {
-		$('#pg-style').load(`css/${pg}.css`, function() {
-			$('#pg-page').load(`pages/${pg}.html`, function() {
-				setCookie('page', pg, 14);
-				document.getElementById('content-container').style.opacity = '1';
-			});
-		});
-	});
+function openNav() {
+	let visible = document.getElementById('visible');
+	document.getElementById(
+		'content-container'
+	).style.marginTop = `-${document.getElementById('nav-bar').clientHeight +
+		$(window).height() * 0.06}px`;
+	visible.style.transition = 'opacity 0.25s';
+	visible.style.opacity = '1';
 }
 
-function sleep(time) {
-	return new Promise(resolve => setTimeout(resolve, time));
-}
-
-function toggleNav(id, bid, t) {
-	let ele = document.getElementById(id);
-	let bar = document.getElementById(bid).clientHeight;
-	let vis = document.getElementById('visible');
-	let offset = -bar + $(window).height() * 0.06;
-
-	if (t) {
-		ele.style.marginTop = `${offset}px`;
-		vis.style.transition = 'opacity 0.25s';
-		vis.style.opacity = '1';
-	} else if (!t) {
-		ele.style.marginTop = '0px';
-		vis.style.opacity = '0';
-		vis.style.transition = 'opacity 0.5s';
-	}
+function closeNav() {
+	let visible = document.getElementById('visible');
+	document.getElementById('content-container').style.marginTop = '0px';
+	visible.style.opacity = '0';
+	visible.style.transition = 'opacity 0.5s';
 }
 
 function setCookie(name, value, days) {
@@ -256,3 +224,50 @@ function loadThemeFromCookies() {
 	if (error != null)
 		document.documentElement.style.setProperty('--error', error);
 }
+
+function sleep(time) {
+	return new Promise(resolve => setTimeout(resolve, time));
+}
+
+// function loadPage(pg) {
+// 	$('#content-container').css('opacity', '0');
+// 	$(this).click(function() {
+// 		$.ajax({
+// 			url: `pages/${pg}.html`,
+// 			success: function(result) {
+// 				$('#page-container').html(result);
+// 				sleep(500).then(() => {
+// 					$('#content-container').css('opacity', '1');
+// 				});
+// 			},
+// 		});
+// 	});
+// }
+
+$('#nav-link a').click(function(e) {
+	e.preventDefault();
+	closeNav();
+
+	let targetUrl = $(this).attr('href'),
+		targetTitle = $(this).attr('title');
+
+	$('#content-container').css('opacity', '0');
+	$(this).click(function() {
+		$.ajax({
+			url: targetUrl,
+			success: function(result) {
+				$('#page-container').html(result);
+				sleep(500).then(() => {
+					$('#content-container').css('opacity', '1');
+				});
+			},
+		});
+
+		window.history.pushState(
+			{ url: '' + targetUrl + '' },
+			targetTitle,
+			targetUrl
+		);
+		return false;
+	});
+});
